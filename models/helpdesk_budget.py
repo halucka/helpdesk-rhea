1ebe14a2b83769aed3577393cd8e4099b07c61f3
halucka <halucka@gmail.com>
2017-10-09 17:19:30 +0200

    first attempt amount_untaxed as initial budget

--- models/helpdesk_budget.py
+++ models/helpdesk_budget.py
@@ -6,10 +6,9 @@ class HelpdeskBudget(models.Model):
     sale_order_id = fields.Many2one('sale.order', string='Sale Order')
     project_id = fields.Many2one('project.project', string='Project')
 
-    #sale_order_name = fields.Char()
-    sale_order_date = fields.Date()
-    amount = fields.Float()
-    amount_remaining = fields.Float()
+    sale_order_date = fields.Datetime('Order Date', related='sale_order_id.date_order')
+    amount = fields.Float('Total Amount', compute='_compute_amount')
+    amount_remaining = fields.Float('Amount Remaining')
 
     def set_budget(self, sale_order_id, project_id):
         self.sale_order_id = sale_order_id
@@ -23,3 +22,8 @@ class HelpdeskBudget(models.Model):
         self.amount_remaining = self.amount_remaining - spent_budget
         return self
 
+    @api.one
+    def _compute_amount(self):
+        # TODO !!! buggy - returns just zeroes
+        amount = self.sale_order_id.amount_untaxed
+        return amount
