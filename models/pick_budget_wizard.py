9a29626f177f528b87465f6c7fc84a9ac3467ea1
halucka <halucka@gmail.com>
2017-10-25 12:26:32 +0200

    rewrote pick_budget so that it always returns a Budget be it a new one if no other was available

--- models/pick_budget_wizard.py
+++ models/pick_budget_wizard.py
@@ -35,8 +35,9 @@ class PickBudgetWizard(models.TransientModel):
         for record in self.env['account.analytic.line'].search([]):
             date_as_datetime = datetime.strptime(record.date, '%Y-%m-%d')  # for Datetime '%Y-%m-%d %H:%M:%S'
             date_correct = (date_as_datetime > date_from_as_datetime) and (date_as_datetime < midnight_date_until)
-            if date_correct and (record.account_id.name == current_project.name):
-                ts_lst.append((record.id, record.date)) #(record.id, record.name)
+
+            if date_correct and (record.account_id.name == current_project.name) and (record.is_paid == False):
+                ts_lst.append((record.id, record.date))
 
         ts_lst.sort(key=itemgetter(1))  # sort by date
 
@@ -78,52 +79,44 @@ class PickBudgetWizard(models.TransientModel):
 
         # remove budgets that are spent, but not those that
         # still have budget on them or are negative (to be made into Sale Order manually)
-        budget_list = [item for item in budget_list_all if item[1] <> 0]
+        budget_list = [item for item in budget_list_all if item[1] > 0]
+        negative_budgets = [item for item in budget_list_all if item[1] < 0]
 
-        budget_list.sort(key=itemgetter(1))  # sort by amount_remaining
-
-        print "budget_list before"
-        print budget_list
-
-        any_budget = False
-        any_positive = False
-        any_negative = False
-        total_remaining_budget = 0
-
-        if len(budget_list) != 0:
-            any_budget = True
-
-        if not any_budget:
-            print "There is no existing Budget within those dates. We need to make a new Budget from a Sales Order."
-            return False
-
-        if any_budget:
-            for item in budget_list:
-                total_remaining_budget += item[1]
-                if item[1] > 0:
-                    any_positive = True
-                if item[1] < 0:
-                    any_negative = True
-
-        print "The total remaining amount on all budgets within those dates is: ", total_remaining_budget
-
-        if any_positive and not any_negative: # This is the normal scenario
+        if len(budget_list) > 0: # This is the normal scenario
             budget_list.sort(key=itemgetter(2))  # sort by date, then return the oldest
             return budget_list[0][0]
+        else:
+            if len(negative_budgets) > 0:  # We are running credit
+                negative_budgets.sort(key=itemgetter(2))  # sort by date, then return the oldest
+                return negative_budgets[0][0]
+            else:
+                # TODO make new Budget for booking
+                # tasks_from_current_project = self.env['project.task'].search([('project_id', '=', current_project.id)])
+                # if len(tasks_from_current_project) > 0:
+                #     partner_id = tasks_from_current_project[0].partner_id
+                #
+                # if not partner_id:
+                #     raise UserError("Please fill in Customer for the Project, then retry.")
+                #
+                # # make a corresponding Sale Order
+                # new_so = self.env["sale.order"]
+                # so_to_write = {'name': "SO Helpdesk Budget",
+                #                'partner_id': partner_id,
+                #                # 'date_order': date.today(),
+                #                }
+                # new_sale_order = new_so.create(so_to_write)
 
-        if any_positive and any_negative:
-            print "There are both positive and negative Budgets..."
-            return False
+                # make a new empty Budget
+                new_budget = self.env["helpdesk.budget"]
 
-        if any_negative and not any_positive:
-            raise UserError(_("We are already running credit (remaining Budget < 0)"))
-            return False
+                budget_to_write = {#'sale_order_id': new_sale_order,
+                                   'project_id': current_project.id,
+                                   'amount': 0,
+                                   'amount_remaining': 0,
+                                   }
 
-        # TODO
-        # try to pick first budget with amount_remaining > 0 and amount not equal to amount_remaining -> i.e open budget
-        # if None, then the budget with amount remaining > 0 and oldest by date
-        # check if there is already a Budget with amount remaining < 0
-        # if still None make new budget (to be manually made into new Sales Order)
+                new_helpdesk_budget = new_budget.create(budget_to_write)
+                return new_helpdesk_budget.id
 
 
     @api.multi
@@ -141,7 +134,13 @@ class PickBudgetWizard(models.TransientModel):
         open_budget_id = self.pick_budget(current_project)
         open_budget = self.env["helpdesk.budget"].search([('id', '=', open_budget_id)])
 
+
         while len(consolidated_timesheets) > 0:
+
+            if open_budget.amount_remaining <= 0:
+                is_running_credit = True
+            else:
+                is_running_credit = False
 
             timesheet_tup = consolidated_timesheets[0]
             # timesheet_tup is (date, cost, orig_timesheets)
@@ -157,7 +156,8 @@ class PickBudgetWizard(models.TransientModel):
             # check if the consolidated timesheet still fits in the budget
             amount_to_transfer = None
             new_amount_remaining = open_budget.amount_remaining - timesheet_cost
-            if new_amount_remaining < 0.0:
+            # if already running credit, it's okay to go further negative
+            if (new_amount_remaining < 0.0) and not is_running_credit:
                 amount_to_transfer = -new_amount_remaining
                 new_amount_remaining = 0
 
@@ -173,49 +173,16 @@ class PickBudgetWizard(models.TransientModel):
 
             for id in timesheet_ids:
                 timesheet = self.env["account.analytic.line"].search([('id', '=', id)])
-                timesheet.write({"budget_debit_ids": (4, new_bd.id)}) # TODO
+                timesheet.write({"budget_debit_ids": (4, new_bd.id),
+                                 "is_paid": True})
 
-            open_budget.write({"amount_remaining" : new_amount_remaining}) # This does not seem to work correctly
+            open_budget.write({"amount_remaining" : new_amount_remaining})
 
             if amount_to_transfer is not None:
                 print "Warning: we need to tranfer to another Budget, this one is used up!"
-
-                consolidated_timesheets[0] = (timesheet_date, amount_to_transfer, timesheet_ids) # make sure this actually modifies the cons timesheets
+                consolidated_timesheets[0] = (timesheet_date, amount_to_transfer, timesheet_ids)
                 open_budget_id = self.pick_budget(current_project)
-
-                if open_budget_id is False:
-                    # TODO remake to make a new negative Budget, not a Timesheet
-
-                    new_name = "Payment for Extra Helpdesk Timesheet(s): " + str(timesheet_ids)
-
-                    tasks_from_current_project = self.env['project.task'].search([('project_id', '=', current_project.id)])
-                    if len(tasks_from_current_project)>0:
-                        partner_id = tasks_from_current_project[0].partner_id
-
-                    if not partner_id:
-                        raise UserError("Please fill in Customer for the Project, then retry.")
-
-                    # make a corresponding Sale Order
-                    new_so = self.env["sale.order"]
-                    so_to_write = {'name': new_name,
-                                   'partner_id': partner_id,
-                                   #'date_order': date.today(),
-                                   }
-                    new_sale_order = new_so.create(so_to_write)
-
-                    # make a new NEGATIVE Budget
-                    new_budget = self.env["helpdesk.budget"]
-
-                    budget_to_write = {'sale_order_id':new_sale_order,
-                                       'project_id': current_project,
-                                       'amount': -amount_to_transfer,
-                                       'amount_remaining': -amount_to_transfer,
-                                        }
-
-                    new_helpdesk_budget = new_budget.create(budget_to_write)
-
-                else:
-                    open_budget = self.env["helpdesk.budget"].search([('id', '=', open_budget_id)])
+                open_budget = self.env["helpdesk.budget"].search([('id', '=', open_budget_id)])
 
             else:
                 del consolidated_timesheets[0]
